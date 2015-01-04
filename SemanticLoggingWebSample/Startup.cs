07992674e2afcf1e2f1f4818714d0611378804d9
lonesomegeek <dugasg.emmanuel@hotmail.com>
2015-01-04 15:09:41 -0500

    First check-in

---
+++ SemanticLoggingWebSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SemanticLoggingWebSample.Startup))]
+namespace SemanticLoggingWebSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
